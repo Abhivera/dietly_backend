6791de48d9faa94a06923b212cb50422215d202a
Abhivera <abhijitverma2016@gmail.com>
2025-07-09 12:05:29 +0530

    f

--- app/services/google_oauth_service.py
+++ app/services/google_oauth_service.py
@@ -7,5 +7,8 @@ oauth.register(
     client_id=settings.google_client_id,
     client_secret=settings.google_client_secret,
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={'scope': 'openid email profile'},
+    client_kwargs={
+        'scope': 'openid email profile',
+        'response_type': 'code'
+    },
 ) 
