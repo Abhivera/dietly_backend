540f8b3884d84b66fa9a8885577ba35004d97090
Abhivera <abhijitverma2016@gmail.com>
2025-07-09 11:57:44 +0530

    fix error

--- app/services/google_oauth_service.py
+++ app/services/google_oauth_service.py
@@ -6,10 +6,6 @@ oauth.register(
     name='google',
     client_id=settings.google_client_id,
     client_secret=settings.google_client_secret,
-    access_token_url='https://oauth2.googleapis.com/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v2/',
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'},
 ) 
